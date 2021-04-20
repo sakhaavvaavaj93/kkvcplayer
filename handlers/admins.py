0825ec6986f2c154a57e53fe814b9fd4156e6794
Sreekumar <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-04-21 03:15:15 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -77,4 +77,4 @@ async def skip(client: Client, message: Message):
 @admins_only
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    await message.reply_text("bot restarted")
+    await message.reply_text("ആരും പേടിക്കണ്ട ..... ഓടിക്കോ")
